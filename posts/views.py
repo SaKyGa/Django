016cb270ada9f32b20e2612116097446beffd039
SaKyGa <s62638320@gmail.com>
2025-04-18 14:41:39 +0300

    HW4

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 import random
 from posts.models import Post
+from posts.forms import PostForm
 
 # Create your views here.
 
@@ -8,7 +9,7 @@ from posts.models import Post
 def test_view(request):
     return HttpResponse(f"Hello, world {random.randint(1, 100)}!")
 
-def html_view(request):
+def home_html_view(request):
     return render(request, "base.html")
 
 
@@ -20,4 +21,19 @@ def post_list_view(request):
 def post_detail_view(request, post_id):
     post = Post.objects.get(id=post_id)
     return render(request, "post/post_detail.html", {"post": post})
+
+
+def create_post_view(request):
+    if request.method == "GET":
+        form = PostForm()
+        return render(request, "post/post_create.html", context={"form": form})
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if not form.is_valid():
+            return render(request, "post/post_create.html", context={"form": form})
+        elif form.is_valid():
+            tags = form.cleaned_data.pop("tags")
+            post = Post.objects.create(**form.cleaned_data)
+            post.tags.set(tags)
+            return redirect("/posts/create/")
 
